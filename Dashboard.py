cf4e41be260654613134ecea23962722fb39ad76
Fernando-Almeida23 <134293342+Fernando-Almeida23@users.noreply.github.com>
2023-08-04 11:25:15 -0300

    excel

--- Dashboard.py
+++ Dashboard.py
@@ -7,7 +7,9 @@ st.set_page_config(layout= 'wide')
 st.title('Pelada de Sábado')
 
 # importar os dados
-tabela = pd.read_excel(r'https://github.com/Fernando-Almeida23/Pelada_sabado/blob/main/pelada%20sabado%2002%20semestre.xlsx')
+url = 'https://github.com/Fernando-Almeida23/Pelada_sabado/blob/main/pelada%20sabado%2002%20semestre.xlsx'
+planilha = 'Jogadores'
+tabela = pd.read_excel(url, planilha)
 tabela.head()
 
 # # Arrumando tabela
