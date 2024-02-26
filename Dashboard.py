6fa111cab992dc2c05a18ca252fcf46d7e011541
Fernando-Almeida23 <134293342+Fernando-Almeida23@users.noreply.github.com>
2024-02-26 08:38:46 -0300

    26/02

--- Dashboard.py
+++ Dashboard.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.set_page_config(layout= 'wide')
 
 st.title('Pelada de Sábado')
-st.write('Última Atualização 05/02/2024')
+st.write('Última Atualização 26/02/2024')
 
 
 # importar os dados
